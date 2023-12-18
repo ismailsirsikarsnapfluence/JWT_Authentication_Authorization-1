cf3aaf1b4a74c38b94955248f19b287017426d43
saif sayyed <sayyedsaif315@gmail.com>
2023-12-17 17:36:06 -0800

    Add project files.

---
+++ JWT_Authentication_Authorization/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using JWT_Authentication_Authorization.Models;
+using Microsoft.AspNetCore.Identity.Data;
+namespace JWT_Authentication_Authorization.Interfaces
+{
+    public interface IAuthService
+    {
+        User AddUser(User user);
+        string Login(Models.LoginRequest loginRequest);
+        Role AddRole(Role role);
+        bool AssignRoleToUser(AddUserRole obj);
+    }
+}
